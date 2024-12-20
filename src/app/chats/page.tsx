38ce80765b54403c8d87e0608cc568c1092f99b0
pritam1322 <pritamjat98@gmail.com>
2024-12-20 16:50:23 +0530

    commit

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -6,9 +6,10 @@ export default function ChatsPage() {
   return (
     <div className=" flex justify-center mt-16 ">
       <div className="w-full border p-8 rounded-xl max-w-lg">
-        <h1 className="btn  btn-accent justify-center flex mx-auto max-w-xs">
+        <h1 className="btn  btn-accent justify-center flex mx-auto max-w-xs text-lg font-inter text-white">
           AI Chatbot
         </h1>
+        <h3 className="text-center text-sm">Ask all your queries to our new chatbot<br/> will help you in each case.</h3>
         <ChatBox />
       </div>
     </div>
